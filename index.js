8c95272e52400170f3fb6e733fa1cb88e2b15753
carmalou <carmenbourlonlong@gmail.com>
2018-05-29 23:38:15 -0500

    one endpoint out

---
+++ index.js
@@ -0,0 +1,52 @@
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port: process.env.PORT || 3000,
+    host: process.env.PORT ? '0.0.0.0' : 'localhost'
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function(request, h) {
+        return 'Welcome to FayePI! ';
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/getromcoms',
+    handler: function(request, h) {
+        var romcoms = require('./data/rom-coms.movies.json');
+        return romcoms
+    }
+});
+
+// server.route({
+//     method: 'GET',
+//     path: '/romcoms/year/{year}',
+//     handler: function(request, h) {
+//         var romcoms = require('./data/rom-coms.movies.json');
+//         var year = encodeURIComponent(request.params.year);
+
+//         var returnYears = _.find(romcoms, function(e) {
+//             return e.year_released == year;
+//         });
+
+//         return returnYears;
+//     }
+// });
